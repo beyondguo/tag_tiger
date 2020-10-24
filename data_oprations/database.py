0700a50856d11fba2d99c455fab6430644d415fa
buyiyang <guo_biyang@163.com>
2020-10-24 09:48:10 +0800

    modified database operations

--- data_oprations/database.py
+++ data_oprations/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-config_path = os.path.join(os.path.dirname(__file__),'../','../','sql_config')
-with open(config_path) as f:
+sql_config_path = os.path.join(os.path.dirname(__file__),'../','../','sql_config')
+with open(sql_config_path) as f:
     sql_str = f.read()
 
 engine = create_engine(sql_str,pool_size=15,pool_recycle=60)
