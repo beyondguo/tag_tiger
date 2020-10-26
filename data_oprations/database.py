086c0c7d6dd77ed9d583780f09e3ca13920351f4
buyiyang <guo_biyang@163.com>
2020-10-26 16:22:19 +0800

    database echo sql

--- data_oprations/database.py
+++ data_oprations/database.py
@@ -7,6 +7,6 @@ sql_config_path = os.path.join(os.path.dirname(__file__),'../','../','sql_config
 with open(sql_config_path) as f:
     sql_str = f.read()
 
-engine = create_engine(sql_str,pool_size=15,pool_recycle=60)
+engine = create_engine(sql_str,pool_size=15,pool_recycle=60, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
